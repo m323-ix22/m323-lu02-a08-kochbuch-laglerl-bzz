497aa4405a3c72ec6eec1f003ee70fa405aaf2cb
laglerl-bzz <laglerl@bzz.ch>
2024-09-02 09:12:50 +0200

    finished

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-# Dein Code kommt hier hin
+import json
+"""
+    Solved Recipe adjustment
+"""
+
+
+def load_recipe(recipe):
+    return json.loads(recipe)
+
+
+def adjust_recipe(recipe, persons):
+    factor = persons / recipe['servings']
+    adjusted_ingredients = {ingredient: amount * factor for ingredient, amount in recipe['ingredients'].items()}
+    return {
+        'title': recipe['title'],
+        'ingredients': adjusted_ingredients,
+        'servings': persons
+    }
+
 
 if __name__ == '__main__':
     # Beispiel für die Datenstruktur eines Rezepts
-    recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
-    # Dein Code kommt hier hin
+    recipe_json = ('{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, '
+                   '"Minced Meat": 500}, "servings": 4}')
